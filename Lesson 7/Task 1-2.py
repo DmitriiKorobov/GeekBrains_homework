f81770fc6a9750cbeeed00ba5a606d744ef5cf48
Dmitrii Korobov <MacOS@MacBook-Pro-MacOS.local>
2022-06-29 01:37:48 +0300

    Complete Lesson 7

---
+++ Lesson 7/Task 1-2.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+from pprint import pprint
+
+
+def make_dir():
+    with open('config.yaml', 'r', encoding='utf-8') as f:
+        data = yaml.safe_load(f)
+
+    pprint(data)
+
+    for key in data:
+        if not os.path.exists(key):
+            os.mkdir(key)
+            pth = os.getcwd()
+            if data[key] is not None:
+                for k in data[key]:
+                    if k[-1:] == 'y':
+                        os.chdir(key)
+                        with open(f'{k}', 'a', encoding='utf-8') as f:
+                            f.writelines('Successful')
+                        os.chdir(pth)
+                    elif k[-1:] == 'l':
+                        os.chdir(key)
+                        with open(f'{k}', 'a', encoding='utf-8') as f:
+                            f.writelines('Successful')
+                        os.chdir(pth)
+                    else:
+                        os.chdir(key)
+                        os.mkdir(k)
+                        os.chdir(pth)
+
+
+if __name__ == "__main__":
+    make_dir()
